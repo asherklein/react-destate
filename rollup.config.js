6eebde1038534616f4ffa300ed7332632396f41d
Asher <aklein@descriptsw.com>
2019-03-04 18:51:24 -0500

    Build to index.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel';
 export default {
     input: 'src/index.js',
     output: {
-        file: 'dist/bundle.js',
+        file: 'dist/index.js',
         format: 'cjs'
     },
     // All the used libs needs to be here
